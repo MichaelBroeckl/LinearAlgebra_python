f7440c0d7ea44062130910913ef98afc353018c2
Michael Broeckl <188877760+MichaelBroeckl@users.noreply.github.com>
2025-03-17 20:42:23 +0100

    Added det calculation

--- gauss_elimination.py
+++ gauss_elimination.py
@@ -1,12 +1,13 @@
 class Matrix:
     """ 
     Expects the dimensions of the coefficient matrix m as the number of rows and n as the number of columns as int and
-    A, which can be an coefficient- or extended coefficient matrix in the form of list of lists and initializes
+    A, (which doesn't work for extended coefficient matricesyet) in the form of list of lists and initializes
     the indices k=0, i=0 and j=0 and the variable for counting the rowswaps l=0.
 
     """
 
     def __init__(self, m, n, A):
+        self.det_sum = 1
         self.step = 0
         self.l = 0
         self.k = 0
@@ -25,7 +26,7 @@ class Matrix:
             if self.k == None:
                 self.j +=1 
                 if self.j > self.n:
-                    return self.A
+                    return self.A, self.l, self.det()
                 else:
                     continue
             if self.k != self.i:
@@ -39,7 +40,7 @@ class Matrix:
             for row in self.A:
                 print(row)
             if self.i >= self.m - 1 or self.j > self.n - 1:
-                return self.A, self.l
+                return self.A, self.l, self.det()
 
     def find_smallest_k(self):
         self.k = None
@@ -54,6 +55,14 @@ class Matrix:
                 factor = self.A[k][self.j] / self.A[self.i][self.j]
                 for x in range(self.j, self.n):
                     self.A[k][x] += self.A[self.i][x] * (-1) * factor 
+    
+
+    def det(self):
+        number_of_nonzero_rows = sum(not all(x == 0 for x in sublist[:self.n]) for sublist in self.A)
+        for i in range(number_of_nonzero_rows):
+            self.det_sum *= self.A[i][i]
+        return (-1) ** self.l * self.det_sum
+
 
     
 
@@ -66,11 +75,22 @@ def main():
             [0, -2, -1, 0],
             [-1, 1, 3, 1],
         ]
-    MatrixA = Matrix(4,4,A)
+    B = [
+        [4, -1],
+        [3, -2],
+    ]
+
+    C = [
+        [1, -2, 1, 1],
+        [0, 1, 1, 1],
+        [1, 0, 1, 0],
+    ]
+    MatrixA = Matrix(3,3,C)
     Ã = MatrixA.gauss_elimination()
     print(f'With l = {Ã[1]} rowswaps, \nthe final matrix in echelon form is:')
     for row in Ã[0]:
         print(row)
+    print(f'And the Determinant det(Ã) = {Ã[2]}')
 
 
 
